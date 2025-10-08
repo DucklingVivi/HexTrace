c7435c30447c7715e7795ee1c1690b193ae6c80d
DucklingVivi <95558019+DucklingVivi@users.noreply.github.com>
2025-10-08 11:18:10 -0400

    idk anymore

--- common/src/main/java/trans/ducklingvivi/hextrace/mixin/IotaTypeMixin.java
+++ common/src/main/java/trans/ducklingvivi/hextrace/mixin/IotaTypeMixin.java
@@ -40,7 +40,7 @@ public class IotaTypeMixin {
         }
         return tag;
     }
-    @WrapMethod(method = "deserialize*")
+    @WrapMethod(method = "deserialize(Lnet/minecraft/nbt/CompoundTag;Lnet/minecraft/server/level/ServerLevel;)Lat/petrak/hexcasting/api/casting/iota/Iota;",remap = false)
     private static Iota hextrace$modifyDeserialize(CompoundTag tag, ServerLevel world, Operation<Iota> original) {
         var iota = original.call(tag, world);
         if(tag.contains(TRACER_TAG)) {
