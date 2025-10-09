23d88a9945cc7717e143e966761139d76b017a99
DucklingVivi <95558019+DucklingVivi@users.noreply.github.com>
2025-10-08 22:32:24 -0400

    Update OperatorBasicMixin.java

--- common/src/main/java/trans/ducklingvivi/hextrace/mixin/OperatorBasicMixin.java
+++ common/src/main/java/trans/ducklingvivi/hextrace/mixin/OperatorBasicMixin.java
@@ -10,6 +10,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import trans.ducklingvivi.hextrace.IIotaDuck;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Collection;
 
@@ -19,8 +20,9 @@ public abstract class OperatorBasicMixin {
     @WrapOperation(method ="operate", at = @At(value = "INVOKE", target = "Lat/petrak/hexcasting/api/casting/arithmetic/operator/OperatorBasic;apply(Ljava/lang/Iterable;Lat/petrak/hexcasting/api/casting/eval/CastingEnvironment;)Ljava/lang/Iterable;",remap = true))
     private static Iterable<Iota> hextrace$logOperator(OperatorBasic instance, Iterable<? extends Iota> iotas, CastingEnvironment castingEnvironment, Operation<Iterable<Iota>> original) {
         var result = original.call(instance, iotas, castingEnvironment);
-        var old = new ArrayList<Iota>((Collection<? extends Iota>) iotas);
-        old.removeAll((Collection<? extends Iota>) result);
+        ArrayList<Iota> old = new ArrayList<>();
+        iotas.forEach(old::add);
+        result.forEach(old::remove);
         old.forEach(iota -> {
             if(((IIotaDuck)iota).isTraced()) {
                 var trace = ((IIotaDuck) iota).getTrace();
