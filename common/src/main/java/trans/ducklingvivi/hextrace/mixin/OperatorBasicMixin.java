cbc20e4ca2b8d9d4179a1bc5d0d3afcd9558fef5
DucklingVivi <95558019+DucklingVivi@users.noreply.github.com>
2025-10-08 23:38:32 -0400

    god knows

--- common/src/main/java/trans/ducklingvivi/hextrace/mixin/OperatorBasicMixin.java
+++ common/src/main/java/trans/ducklingvivi/hextrace/mixin/OperatorBasicMixin.java
@@ -4,8 +4,10 @@ package trans.ducklingvivi.hextrace.mixin;
 import at.petrak.hexcasting.api.casting.arithmetic.operator.OperatorBasic;
 import at.petrak.hexcasting.api.casting.eval.CastingEnvironment;
 import at.petrak.hexcasting.api.casting.iota.Iota;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import trans.ducklingvivi.hextrace.IIotaDuck;
@@ -13,15 +15,14 @@ import trans.ducklingvivi.hextrace.IIotaDuck;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 @Mixin(value = OperatorBasic.class, remap = false)
 public abstract class OperatorBasicMixin {
 
-    @WrapOperation(method ="operate", at = @At(value = "INVOKE", target = "Lat/petrak/hexcasting/api/casting/arithmetic/operator/OperatorBasic;apply(Ljava/lang/Iterable;Lat/petrak/hexcasting/api/casting/eval/CastingEnvironment;)Ljava/lang/Iterable;",remap = true))
-    private static Iterable<Iota> hextrace$logOperator(OperatorBasic instance, Iterable<? extends Iota> iotas, CastingEnvironment castingEnvironment, Operation<Iterable<Iota>> original) {
-        var result = original.call(instance, iotas, castingEnvironment);
-        ArrayList<Iota> old = new ArrayList<>();
-        iotas.forEach(old::add);
+    @ModifyExpressionValue(method ="operate", at = @At(value = "INVOKE", target = "Lat/petrak/hexcasting/api/casting/arithmetic/operator/OperatorBasic;apply(Ljava/lang/Iterable;Lat/petrak/hexcasting/api/casting/eval/CastingEnvironment;)Ljava/lang/Iterable;"))
+    private static Iterable<Iota> hextrace$logOperator(Iterable<Iota> result , @Local(ordinal = 1) List<Iota> iotas) {
+        ArrayList<Iota> old = new ArrayList<>(iotas);
         result.forEach(old::remove);
         old.forEach(iota -> {
             if(((IIotaDuck)iota).isTraced()) {
